6426abc82ce34dfed26c982631548e9f4d5284a7
ANAS-LACENE-NECER <anasln95400@yahoo.com>
2026-06-17 10:52:06 +0200

    Ajout des tests JUnit Mockito et MockMvc

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -1,17 +1,51 @@
 package com.example.demo.data;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
 public class VoitureTest {
 
     @Test
-    void creerVoiture(){
-        assertEquals(1,1);
+    void creerVoitureAvecConstructeur() {
+        Voiture voiture = new Voiture("Ferrari", 2000);
+
+        assertEquals("Ferrari", voiture.getMarque());
+        assertEquals(2000, voiture.getPrix());
     }
 
+    @Test
+    void modifierPrixVoiture() {
+        Voiture voiture = new Voiture("Ferrari", 2000);
+
+        voiture.setPrix(3000);
+
+        assertEquals(3000, voiture.getPrix());
+    }
+
+    @Test
+    void modifierMarqueVoiture() {
+        Voiture voiture = new Voiture();
+
+        voiture.setMarque("Toyota");
+
+        assertEquals("Toyota", voiture.getMarque());
+    }
+
+    @Test
+    void modifierIdVoiture() {
+        Voiture voiture = new Voiture();
+
+        voiture.setId(10);
+
+        assertEquals(10, voiture.getId());
+    }
+
+    @Test
+    void testerToString() {
+        Voiture voiture = new Voiture("BMW", 5000);
+        voiture.setId(1);
+
+        assertEquals("Car{marque='BMW', prix=5000, id=1}", voiture.toString());
+    }
 }
